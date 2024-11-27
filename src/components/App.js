e73e327cd21e82fa6bc25835c4a165266b72580d
NurEdlyana <23016282@myrp.edu.sg>
2024-11-27 23:26:31 +0800

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -103,6 +103,7 @@ function Stats({items}) {
   const totalPacked = items.filter((item) => item.packed).length;   // Call for items array, to count the no. of item that has packed as true
   const compRate = totalItem > 0 ? Math.round((totalPacked/totalItem)*100):0;
   
+  // Display status 
   return (
     <footer className="stats">
       <em>You have {totalItem} items in the list. You already packed {totalPacked} ({compRate}%).</em>
